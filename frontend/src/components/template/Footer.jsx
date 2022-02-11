720e1d18aaeb81c9276bda434bd2f8515d234126
Mongruel <arduinni@gmail.com>
2022-02-11 17:54:08 -0300

    Componentes cabeçalho e rodapé

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import './Footer.css'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido por <i className="fa fa-heart text-danger"></i> 
+            <strong> Cod<span className="text-danger">3</span>r</strong>
+        </span>
     </footer>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import './App.css'
+
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Main from '../components/template/Main'
@@ -9,6 +12,7 @@ export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Main icon="home" title="Início"
+            subtitle="Segundo Projeto do capítulo de React"/>
         <Footer />
     </div>
